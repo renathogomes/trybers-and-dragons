af0e8d8d934ab29a1cfe5517fc97f858df70a86e
renatho <renatho.20gomes@gmail.com>
2024-01-30 12:03:06 -0300

    req 5...

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,9 @@
 import Archetypes from './Arquetypes';
+import Mage from './Mage';
+import Warrior from './Warrior';
+import Necromancer from './Necromancer';
+import Ranger from './Ranger';
 
 export default Archetypes;
+
+export { Mage, Warrior, Necromancer, Ranger };
