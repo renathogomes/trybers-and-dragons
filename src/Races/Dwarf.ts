125c747a3ac6778707d888ba7b5365eb418ca0c2
renatho <renatho.20gomes@gmail.com>
2024-01-30 10:18:23 -0300

    req1

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private static _counter = 0;
+
+  constructor(name: string, dexterity: number, private _maxLifePoints: number = 80) {
+    super(name, dexterity);
+    Dwarf._counter += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Dwarf._counter;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Dwarf;
